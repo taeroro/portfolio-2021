5cc5a63b78e3b2c3fb6b46ffee63644a9bc11605
Ryan Fan <ryanfan1996@gmail.com>
2021-02-08 18:07:17 -0500

    again ugh

--- ryan-web/utils/contentful.js
+++ ryan-web/utils/contentful.js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful'
 
-const IS_PRODUCTION = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF === 'main'
+// const IS_PRODUCTION = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF === 'main'
+const IS_PRODUCTION = true;
 
 export const ContentfulAPI = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
